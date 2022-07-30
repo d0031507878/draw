92d3a77e2a630f5bedfc8b597302ee1581524fc7
d0031507878 <d0031507878@gmail.com.tw>
2022-07-30 23:44:48 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import Draw from "./components/Draw_Item/Draw";
 function App() {
   fetch("https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=175415", {
     method: "GET",
-    // mode: "no-cors",
-    // headers: "Content-Type: text/html ;charset=utf-8",
+    mode: "no-cors",
+    headers: {
+      "Access-Control-Allow-Origin": "https://testro.gnjoy.com.tw",
+    },
+    referrer: "https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=175415",
   })
     .then((response) => {
       console.log(response);
