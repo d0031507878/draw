8ba9ec087a88324852317f72d4f6bfe5c6b53be0
d0031507878 <d0031507878@gmail.com.tw>
2022-07-31 00:07:02 +0800

    update

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import React, { useState, Fragment } from "react";
 import Draw from "./components/Draw_Item/Draw";
 
 function App() {
-  fetch("https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=175415", {
-    method: "GET",
-    mode: "no-cors",
-    headers: {
-      "Access-Control-Allow-Origin": "https://testro.gnjoy.com.tw",
-    },
-    referrer: "https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=175415",
-  })
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((error) => {
-      console.log(`${error}`);
-    });
+  // fetch("https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=175415", {
+  //   method: "GET",
+  //   mode: "no-cors",
+  //   headers: {
+  //     "Access-Control-Allow-Origin": "https://testro.gnjoy.com.tw",
+  //   },
+  //   referrer: "https://ro.gnjoy.com.tw/notice/notice_view.aspx?id=175415",
+  // })
+  //   .then((response) => {
+  //     console.log(response);
+  //   })
+  //   .catch((error) => {
+  //     console.log(`${error}`);
+  //   });
 
   return (
     <React.Fragment>
